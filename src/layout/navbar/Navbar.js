bca82d56672e58fd704b2bff508c314e21086adf
sibi71 <thearunsibi@gmail.com>
2023-11-05 21:20:37 +0530

    update

--- src/layout/navbar/Navbar.js
+++ src/layout/navbar/Navbar.js
@@ -61,7 +61,7 @@ const Navbar = () => {
         <LoginModal modalOpen={modalOpen} setModalOpen={setModalOpen} />
       )}
 
-      <div className="bg-emerald-500 sticky top-0 z-20">
+      <div className="bg-emerald sticky top-0 z-20">
         <div className="max-w-screen-2xl mx-auto px-3 sm:px-10">
           <div className="top-bar h-16 lg:h-auto flex items-center justify-between py-4 mx-auto">
             <Link href="/">

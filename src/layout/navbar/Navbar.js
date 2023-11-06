b44b6eac6222afc09f81f49a0c7a32f54efd96c6
sibi71 <thearunsibi@gmail.com>
2023-11-06 17:14:12 +0530

    update

--- src/layout/navbar/Navbar.js
+++ src/layout/navbar/Navbar.js
@@ -65,7 +65,7 @@ const Navbar = () => {
         <div className="max-w-screen-2xl mx-auto px-3 sm:px-10">
           <div className="top-bar h-16 lg:h-auto flex items-center justify-between py-4 mx-auto">
             <Link href="/">
-              <a className="mr-3 lg:mr-12 xl:mr-12 hidden md:hidden lg:block">
+              <a className="mr-3 lg:mr-12 xl:mr-12  lg:block">
                 <Image
                   width={140}
                   height={35}

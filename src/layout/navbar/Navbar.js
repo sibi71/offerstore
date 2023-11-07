f9f0b9bfc35c41e5442ac7082525883203941a1c
sibi71 <thearunsibi@gmail.com>
2023-11-07 18:37:15 +0530

    update

--- src/layout/navbar/Navbar.js
+++ src/layout/navbar/Navbar.js
@@ -64,9 +64,9 @@ const Navbar = () => {
       <div className="bg-emerald-500 sticky top-0 z-20">
         <div className="max-w-screen-2xl mx-auto px-3 sm:px-10">
           <div className="top-bar h-16 lg:h-auto flex items-center justify-between py-4 mx-auto">
-            <p  className={router.pathname == "/" ? "active" : ""}>
-            <Link href="/" >
-              <a className="mr-3 lg:mr-12 xl:mr-12  lg:block">
+           
+            <Link href="/" className={router.pathname == "/" ? "active" : ""} >
+              <a className="mr-3 lg:mr-12 xl:mr-12  lg:block ">
                 <Image
                   width={135}
                   height={33}
@@ -78,7 +78,7 @@ const Navbar = () => {
                 />
               </a>
             </Link>
-            </p>
+         
             <div className="w-full transition-all duration-200 ease-in-out lg:flex lg:max-w-[520px] xl:max-w-[750px] 2xl:max-w-[900px] md:mx-12 lg:mx-4 xl:mx-0">
               <div className="w-full flex flex-col justify-center flex-shrink-0 relative z-30">
                 <div className="flex flex-col mx-auto w-full">
